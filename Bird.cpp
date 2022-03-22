6720375b7caaed38cd242f0148b2b3fc7ccbb5af
evennybo <evenmyrennybo@gmail.com>
2022-03-21 19:59:35 -0500

    Hey

---
+++ Bird.cpp
@@ -0,0 +1,5 @@
+// Even Myren Nybø
+// Created by even on 3/18/22.
+// Bird class file
+
+#include "Bird.h"
---
+++ Bird.h
@@ -0,0 +1,14 @@
+// Even Myren Nybø
+// Created by even on 3/18/22.
+// bird header
+
+#ifndef CPP_CLASS_DEMO_BIRD_H
+#define CPP_CLASS_DEMO_BIRD_H
+
+
+class bird {
+
+};
+
+
+#endif //CPP_CLASS_DEMO_BIRD_H
